e1145f8ed8a13734cb160a82234fd8626a15ee82
venky hari <harivenky815@gmail.com>
2023-05-22 15:33:10 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>bahubali</h1>
+        <h2>this is the only movie he got recognized</h2>
+    </div>
+  );
+}
+
+export default App;
